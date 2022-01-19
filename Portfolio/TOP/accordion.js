2b907609f84181a1d55bc91272904e1e052b2104
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-01-18 19:28:39 -0800

    --allow-empty-message

--- Portfolio/TOP/accordion.js
+++ Portfolio/TOP/accordion.js
@@ -1,40 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-        const headerAccordion = document.getElementById("header__accordion");
-        headerAccordion.addEventListener("click", accordion);
+    const accordionOpen = document.getElementById("header__accordion--open");
+    accordionOpen.addEventListener("click", showGlovalNab);
 });
 
-function accordion() {
-    const headerAccordion = document.getElementById("header__accordion");
-    const headerGlovalNav = document.getElementById("header__gloval-nav");
+function showGlovalNab() {
+    const accordionOpen = document.getElementById("header__accordion--open");
+    const glovalNav = document.getElementById("header__gloval-nav");
+    const accordionClose = document.getElementById("header__accordion--close");
 
-    if (window.matchMedia( "(max-width: 1023px)" ).matches) {
-        // console.log(headerGlovalNav.style.display);
-        if (headerGlovalNav.style.display === "") {
-            headerGlovalNav.style.display = "flex";
-            // console.log(headerGlovalNav.style.display);
-        } else if (headerGlovalNav.style.display === "flex") {
-            headerGlovalNav.style.display = "none";
-            // console.log(headerGlovalNav.style.display);
-        } else if (headerGlovalNav.style.display === "none") {
-            headerGlovalNav.style.display = "flex";
-            // console.log(headerGlovalNav.style.display);
-        } else if (headerAccordion === null) {
-            headerGlovalNav.style.display = "flex";
-            // console.log(headerGlovalNav.style.display);
-        }
-    } else  {
-        headerGlovalNav.style.display = "flex"
-    }
-};
-
-// window.addEventListener("mousemove", displayGlovalNav);
-
-// function displayGlovalNav() {
-//     const headerAccordion = document.getElementById("header__accordion");
-//     const headerGlovalNav = document.getElementById("header__gloval-nav");
-//     if (headerAccordion.style.display === null ) {
-//         headerGlovalNav.style.display = "flex";
-//     }
-// };
-
-
+    accordionOpen.style.display = "none";
+    glovalNav.style.display = "flex";
+    accordionClose.style.display = "flex";
+}
