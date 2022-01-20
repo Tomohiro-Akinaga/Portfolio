c54ccf875c5e35c38ef2799a33fb9de3901e4508
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-01-19 20:11:41 -0800

    last task:footer

--- Portfolio/TOP/accordion.js
+++ Portfolio/TOP/accordion.js
@@ -1,14 +1,33 @@
 window.addEventListener("DOMContentLoaded", () => {
     const accordionOpen = document.getElementById("header__accordion--open");
-    accordionOpen.addEventListener("click", showGlovalNab);
-});
-
-function showGlovalNab() {
-    const accordionOpen = document.getElementById("header__accordion--open");
-    const glovalNav = document.getElementById("header__gloval-nav");
     const accordionClose = document.getElementById("header__accordion--close");
+    const glovalNav = document.getElementById("header__gloval-nav");
+    accordionOpen.addEventListener("click", addClassName);
+    accordionClose.addEventListener("click", addClassName);
 
-    accordionOpen.style.display = "none";
-    glovalNav.style.display = "flex";
-    accordionClose.style.display = "flex";
-}
+
+    function addClassName() {
+        if(glovalNav.classList.contains("is-active")) {
+            glovalNav.classList.remove("is-active")
+        } else {
+            glovalNav.classList.add("is-active")
+
+        }
+
+        if(accordionOpen.classList.contains("inactive")) {
+            accordionOpen.classList.remove("inactive")
+        } else {
+            accordionOpen.classList.add("inactive")
+
+        }
+
+        if(accordionClose.classList.contains("is-active")) {
+            accordionClose.classList.remove("is-active")
+        } else {
+            accordionClose.classList.add("is-active")
+
+        }
+    }
+    
+
+});
