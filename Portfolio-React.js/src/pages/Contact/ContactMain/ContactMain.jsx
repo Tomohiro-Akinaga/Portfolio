468b2289ba04c972bf250d4523444ee9560727d9
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2022-03-26 16:04:10 -0700

    done contact page

--- Portfolio-React.js/src/pages/Contact/ContactMain/ContactMain.jsx
+++ Portfolio-React.js/src/pages/Contact/ContactMain/ContactMain.jsx
@@ -1,5 +1,19 @@
+import styles from "./ContactMain.module.scss";
+
 function ContactMain() {
-    return <main>ContactMain</main>;
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>
+                Thank you for visiting my portfolio !
+            </h1>
+            <p className={styles.text}>
+                If you require any further information, feel free to contact me :)
+            </p>
+            <a href="mailto:tomohiroakinaga@gmail.com" className={styles.email}>
+                tomohiroakinaga@gmail.com
+            </a>
+        </main>
+    );
 }
 
 export default ContactMain;
